c8774eb9ec2f76bad30b0d6840a9cab8bbafdcd0
EuronRamadani <73657377+EuronRamadani@users.noreply.github.com>
2025-01-12 18:22:35 +0100

    fixed pagination icons

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -327,7 +327,19 @@ const DashboardPage = () => {
                 disabled={currentPage === 1}
                 className="btn btn-outline-primary"
               >
-                Previous
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-chevron-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+                  />
+                </svg>
               </button>
               <span className="text-muted">
                 {currentPage} of {totalPages}
@@ -337,7 +349,19 @@ const DashboardPage = () => {
                 disabled={currentPage === totalPages}
                 className="btn btn-outline-primary"
               >
-                Next
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-chevron-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+                  />
+                </svg>
               </button>
             </div>
           </div>
